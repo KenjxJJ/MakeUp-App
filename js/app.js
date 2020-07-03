61f35dd5917f53f6bdfcb05b1bef35b4ac3efe4f
JOEL KIGGWE <joelkiggwe94@gmail.com>
2020-07-03 22:16:11 +0300

    Add responsive navicon

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 'use strict'
 
 // Fetch items 
-async function fetchProducts(){
-      
-     const itemsResponse = await fetch('./data/products.json');
-     const products =  await itemsResponse.json();
-      displayProducts(products);
+async function fetchProducts() {
+
+    const itemsResponse = await fetch('./data/products.json');
+    const products = await itemsResponse.json();
+    displayProducts(products);
 };
 
 function displayProducts(products) {
-   const displayByID = document.querySelector('.products');
-         
-    displayByID.innerHTML =  products.map( product =>  ` 
+    const displayByID = document.querySelector('.products');
+
+    displayByID.innerHTML = products.map(product => ` 
     
         <div class='product'>
          <img class="displayed_image" src=${product.image_link} alt="">
@@ -26,3 +26,24 @@ function displayProducts(products) {
 }
 
 fetchProducts();
+
+
+
+// Hamburger Icon
+
+document.getElementById('myHamburger').addEventListener('click', openNav, true);
+document.querySelector('.closebtn').addEventListener('click', closeNav, false);
+
+function openNav() {
+    document.querySelector('.header').style.width = "55%";
+    // document.querySelector(".main").style.marginLeft = "250px";
+    //   For opacity choice
+    document.body.style.backgroundColor = " rgba(252, 252, 248, 0.6);";
+}
+
+function closeNav() {
+    document.querySelector(".header").style.width = "0";
+    document.querySelector(".main").style.marginLeft = "0";
+    // For opacity choice
+    document.body.style.backgroundColor = "white";
+}
