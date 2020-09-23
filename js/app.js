d8e90302cd31704596c8f92c5d35aea514a3d006
JOEL KIGGWE <joelkiggwe94@gmail.com>
2020-09-23 22:41:26 +0300

    Fix the IntersectionObserver API

--- js/app.js
+++ js/app.js
@@ -3,9 +3,8 @@ const displayProducts = (products) => {
     const displayByID = document.querySelector('.products');
 
     displayByID.innerHTML = products.map(product => ` 
-        
-            <div class='product'>
-             <img class="displayed_image" data-src=${product.image_link} alt="">
+           <div class='product'>
+             <img class="displayed_image" data-src=${product.image_link} alt=""/>
              <div class='about_product'>
                     Name : ${product.name}<br>
                     Currency : ${product.currency}<br>
@@ -18,10 +17,10 @@ const displayProducts = (products) => {
 
 // Fetch items 
 async function fetchProducts() {
-
     const itemsResponse = await fetch('https://makeup-api.herokuapp.com/api/v1/products.json');
     const products = await itemsResponse.json();
     displayProducts(products);
+    lazyLoad();
 };
 
 
@@ -45,31 +44,32 @@ function closeNav() {
 }
 
 // lazy load images
+function lazyLoad() {
+    const images = document.querySelectorAll('[data-src]');
+    console.log(images);
+    const config = {
+        rootMargin: '0px 0px 50px 0px',
+        threshold: 0.5
+    };
 
-function preloadImage(img) {
-    const src = img.getAttribute('data-src');
-    if (!src) { return; }
-    img.src = src;
-}
+    let observer = new IntersectionObserver(function(entries, self) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                console.log(`Image ${entry.target.src} is in the viewport!`);
+                preloadImage(entry.target);
+                // Stop watching and load the image
+                self.unobserve(entry.target);
+            }
+        });
+    }, config);
 
-const images = document.querySelectorAll('[data-src]');
-const config = {
-    rootMargin: '0px 0px 50px 0px',
-    threshold: [0, 0.2, 0.5, 1]
-};
-let loaded = 0;
-
-let observer = new IntersectionObserver(function(entries, self) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            // console.log(`Image ${entry.target.src} is in the viewport!`);
-            preloadImage(entry.target);
-            // Stop watching and load the image
-            self.unobserve(entry.target);
-        }
+    images.forEach(image => {
+        observer.observe(image);
     });
-}, config);
 
-images.forEach(image => {
-    observer.observe(image);
-});
+    function preloadImage(img) {
+        const src = img.getAttribute('data-src');
+        if (!src) { return; }
+        img.src = src;
+    }
+} // end function lazyLoad
